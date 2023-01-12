37aaa0e1647d2f197934796123d608bcd793293f
Basiel Makled <basiel1234@gmail.com>
2023-01-12 15:33:45 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1 className = 'App'>Hello Dojo!</h1>
+    <h2 className = 'App'>Things I Need to Do:</h2>
+    <ul className = 'App'>*Learn react</ul>
+    <ul className = 'App'>*Climb Mt. Everest</ul>
+    <ul className = 'App'>*run a marathon</ul>
+    <ul className = 'App'>*Feed the dogs</ul>
+    </>
   );
 }
 
